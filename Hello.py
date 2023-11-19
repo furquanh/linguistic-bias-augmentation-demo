316a5036f55f71a92368e443c6ef4f5a22ada744
Muhammad Furquan Hassan <64953180+furquanh@users.noreply.github.com>
2023-11-19 09:14:46 +0000

    Final app

--- Hello.py
+++ Hello.py
@@ -14,38 +14,121 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from openai import OpenAI
+import pandas as pd
 
 LOGGER = get_logger(__name__)
 
+def load_submissions():
+    try:
+        return pd.read_csv("submissions.csv").reset_index(drop=True)
+    except FileNotFoundError:
+        # Return an empty DataFrame if the file doesn't exist
+        return pd.DataFrame(columns=["Augmentation Name", "Explanation"]).reset_index(drop=True)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+@st.cache_data(persist="disk")    
+def add_submission(submissions, entry):
+    new_entry = pd.DataFrame([entry])
+    updated_submissions = pd.concat([submissions, new_entry], axis=0, ignore_index=True)
+    updated_submissions.to_csv("submissions.csv", index=False)
+    return updated_submissions
+
+@st.cache_resource
+def get_client():
+    client = OpenAI()
+    return client
+
+def augment_text(augmentation_type, prompt, client):
+ with st.spinner(f'Applying {augmentation_type}...'):   
+    response = client.chat.completions.create(
+      model="gpt-4-1106-preview",
+      messages=[
+        {"role": "system", "content": "You are an expert linguist. Do not include explaination, only reply back with the sentence."},
+        {"role": "user", "content": prompt},
+      ]
     )
+    return response.choices[0].message.content
 
-    st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+def main():
+    st.title('Linguistic Bias Dataset Project - Sentence Augmentation Demo')
+    sentence = st.text_area('Enter a sentence:', height=100)
+    augment_button = st.button('Generate Augmentations')
+    client = get_client()
+    submissions = load_submissions()
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
+    if augment_button and sentence:
+          prompt_map = {"African American English": "Convert the following sentence to African American Vernacular English: ", 
+            "Filler Words": "Insert filler words into this sentence: ",
+            "Hashtags": "Add hashtags into this sentence to mimic language on twitter: ",
+            "Emojify ": "Add emojis the following sentence mimicing language used in texting and social media: ",
+            "Formalize": "Convert the text style from informal to formal english: ",
+            "Misspelling": "Insert common misspelling within this sentence: ",
+            "Mixed Language": "Pick a single word from this sentece and replace it with it's translation to one other language, mimicing a mistake a bilingual person might do: ",
+            "Subject-Verb Agreement Errors": "Modify the sentence it so that the subject and verb do not agree in number: ",
+            "Run-on Sentences and Comma Splices": "Transform the following sentence into a run-on sentence or a comma splice: ",
+            "Sentence Fragments": "Convert this sentence into a sentence fragment by removing essential elements: ",
+            "Incorrect Tense Use": "Change the tense in the following sentence inappropriately: ",
+            "Misplaced or Dangling Modifiers": "Rearrange the following sentence to create a misplaced or dangling modifier: ",
+            "Wrong Word Order": "Rearrange the words in this sentence into an incorrect but syntactically possible order: ",
+            "Pronoun-Antecedent Agreement Errors": "modify this sentence so that the pronouns do not agree in number with their antecedents: ",
+            "Incorrect Use of Articles": "modify this sentence so to show incorrect use of articles: ",
+            "Mixed Constructions": "Change this sentence to have mixed constructions, starting in one grammatical structure and ending in another: "
+            }
+
+
+          augmented_sentences = []
+          progress_text = "Applying augmentations. Please wait."
+          my_bar = st.progress(0, text=progress_text)
+          i = 10
+          for augmentation_type, prompt_format in prompt_map.items():
+              prompt = prompt_format + sentence
+              augmented_sentence = augment_text(augmentation_type, prompt, client)
+              my_bar.progress(i, text=progress_text)
+              i += 10
+              augmented_sentences.append((augmentation_type, augmented_sentence))
+          my_bar.empty()
+          df = pd.DataFrame(augmented_sentences, columns=['Augmentation Type', 'Augmented Text'])
+          st.table(df)
+
+          with st.expander("See explanation"):
+            st.markdown('''**Subject-Verb Agreement Errors**: Occur when the verb form does not agree with the subject in number (singular or plural). For example, "She walk to school every day" instead of "She walks to school every day."
+
+    **Run-on Sentences and Comma Splices**: These happen when two or more independent clauses are incorrectly joined without proper punctuation or conjunction. For example, "I went shopping I bought a dress."
+
+    **Sentence Fragments**: This error involves incomplete sentences that lack either a subject, a verb, or a complete thought. For instance, "Because I went to the store."
+                        
+    **Incorrect Tense Use:** Using the wrong tense can lead to syntactic confusion, like using past tense instead of present, or vice versa.
+
+    **Misplaced or Dangling Modifiers**: These mistakes occur when a modifier (a word, phrase, or clause that describes something else) is not clearly or logically related to the word it modifies. For example, "Running quickly, the goal seemed impossible to reach" (misplaced modifier).
+
+    **Wrong Word Order**: English typically follows a Subject-Verb-Object (SVO) order. Deviations can cause confusion, e.g., "The cat the mouse chased."
+
+    **Pronoun-Antecedent Agreement Errors**: This occurs when a pronoun does not agree in number with its antecedent. For example, "Every student must bring their pencil." ('Their' should be 'his or her' to agree with 'every student').
+
+    **Incorrect Use of Articles**: Mistakes involving the use of 'a', 'an', and 'the' can affect sentence structure and meaning.
+
+    **Mixed Constructions**: These errors happen when a sentence starts with one construction and then abruptly changes to another, leading to confusion.''')
+
+    ##
+    st.header("Suggest a New Text Augmentation")
+    st.write("The augmentation should be syntatic, think of the common mistakes people do when writing english.")
+    with st.form(key='augmentation_form'):
+        aug_name = st.text_input("Augmentation Name")
+        aug_reason = st.text_area("Explain why", height=100)
+        submit_button = st.form_submit_button(label='Submit')
+
+        if submit_button and aug_name and aug_reason:
+            new_submission = {"Augmentation Name": aug_name, "Explanation": aug_reason}
+            submissions = add_submission(submissions, new_submission)
+            #e#dited_df.append(new_submission, ignore_index=True)
+            st.success("Thank you for your suggestion!")
+            time.sleep(2)
+            st.rerun()
+
+    st.header("Suggested Augmentations:")
+    st.dataframe(submissions, use_container_width=True)
 
 if __name__ == "__main__":
-    run()
+    main()
